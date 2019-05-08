9942cefb1bc57f8ac436c629a474cb9442ec2c2c
metchegaray <maricel.etchegaray@gmail.com>
2019-05-07 21:10:10 -0300

    prueba

--- src/main/java/com/iclonet/carteleriasegura/aplicacion/MainApp.java
+++ src/main/java/com/iclonet/carteleriasegura/aplicacion/MainApp.java
@@ -7,7 +7,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+/**
+ * Comentario
+ * @author Usuario
+ *
+ */
 public class MainApp extends Application {
 
     @Override
